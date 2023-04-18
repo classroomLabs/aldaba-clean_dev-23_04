a62aebd7f3fe8676190523ef64a51a115175e672
Alberto Basalo <albertobasalo@hotmail.com>
2023-04-18 12:14:30 +0200

    feat: naming classroom

--- src/1-naming/2-meaningful.ts
+++ src/1-naming/2-meaningful.ts
@@ -1,8 +1,18 @@
 // ❌
+// function getClient() {
+//   return "Alice";
+// }
+// function readProvider() {
+//   return "Bob";
+// } // 🤢 get is the same as read?
+// function postCustomer(name: string) {} // 🤢 customer is the same as client?
+
+// ✅
 function getClient() {
   return "Alice";
 }
-function readProvider() {
+// 😏 same action, same verb
+function getProvider() {
   return "Bob";
-} // 🤢 get is the same as read?
-function postCustomer(name: string) {} // 🤢 customer is the same as client?
+}
+function postClient(name: string) {} // 😏 a client is always client
--- src/1-naming/4-style.ts
+++ src/1-naming/4-style.ts
@@ -1,12 +1,26 @@
 // ❌
-const created_at = new Date(); // 🤢 avoid symbols
-const workingDays = 5; // 🤢 a cons primitive is a CONSTANT
-function Calculate_payroll() {
-  // 🤢 oh lord...
+// const created_at = new Date(); // 🤢 avoid symbols
+// const workingDays = 5; // 🤢 a cons primitive is a CONSTANT
+// function Calculate_payroll(){
+
+//   // 🤢 oh lord...
+// }
+// class employee {
+//   // 🤢 type definition should be PascalCase
+// }
+// interface IPayable {
+//   // 🤢 an interface is not a class; find a good name
+// }
+
+// ✅
+const createdAt = new Date(); // 😏 camelCaseIsEasyToRead
+const WORKING_DAYS = 5; // 😏 CONSTANTS SHOULD SCREAM
+function calculatePayroll() {
+  // 😏 function naming is naming
 }
-class employee {
-  // 🤢 type definition should be PascalCase
+class Employee {
+  // 😏 Type Definition
 }
-interface IPayable {
-  // 🤢 an interface is not a class; find a good name
+interface Pay {
+  // 😏 is about behavior
 }
